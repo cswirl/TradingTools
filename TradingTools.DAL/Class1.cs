601a411e0c717746b660a529b49cbedb5fc95d30
cswirl <jay.angeles@protonmail.com>
2021-09-27 16:02:58 -0400

    Add DAL. Create CalculatorState class

---
+++ TradingTools.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TradingTools.DAL
+{
+    public class Class1
+    {
+    }
+}
---
+++ TradingTools.Trunk/Entity/CalculatorState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingTools.Trunk.Entity
+{
+    class CalculatorState
+    {
+        public decimal Capital { get; set; }
+        public decimal Leverage { get; set; }
+        public decimal EntryPriceAvg { get; set; }
+        public int DayCount { get; set; }
+        public decimal DailyInterestRate { get; set; }
+        public decimal PriceIncreaseTarget { get; set; }
+        public decimal PriceDecreaseTarget { get; set; }
+        public decimal PEP_ExitPrice { get; set; }
+        public string PEP_Note { get; set; }
+        public decimal LEP_ExitPrice { get; set; }
+        public string LEP_Note { get; set; }
+        public string Ticker { get; set; }
+        public string Strategy { get; set; }
+        public string Note { get; set; }
+    }
+}
