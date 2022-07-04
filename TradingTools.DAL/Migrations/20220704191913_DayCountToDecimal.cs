b95915d622e3daf694593b7689bd3a5cef79049c
cswirl <jay.angeles@protonmail.com>
2022-07-04 15:22:59 -0400

    Add Migration: Change DayCount to Decimal - database updated

---
+++ TradingTools.DAL/Migrations/20220704191913_DayCountToDecimal.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TradingTools.DAL.Migrations
+{
+    public partial class DayCountToDecimal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "DayCount",
+                table: "Trades",
+                type: "decimal(18,6)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "DayCount",
+                table: "CalculatorStates",
+                type: "decimal(18,6)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DayCount",
+                table: "Trades",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DayCount",
+                table: "CalculatorStates",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldNullable: true);
+        }
+    }
+}
