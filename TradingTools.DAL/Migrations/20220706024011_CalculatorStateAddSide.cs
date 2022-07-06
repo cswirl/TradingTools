1a9c888470d5bbb565174e746aca83c9a89e6a62
cswirl <jay.angeles@protonmail.com>
2022-07-05 23:06:32 -0400

    Add Migration: CalculatorState add properties: Side, IsLotSizeChecked

---
+++ TradingTools.DAL/Migrations/20220706024011_CalculatorStateAddSide.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TradingTools.DAL.Migrations
+{
+    public partial class CalculatorStateAddSide : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PositionSide",
+                table: "Trades",
+                newName: "Side");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLotSizeChecked",
+                table: "CalculatorStates",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Side",
+                table: "CalculatorStates",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsLotSizeChecked",
+                table: "CalculatorStates");
+
+            migrationBuilder.DropColumn(
+                name: "Side",
+                table: "CalculatorStates");
+
+            migrationBuilder.RenameColumn(
+                name: "Side",
+                table: "Trades",
+                newName: "PositionSide");
+        }
+    }
+}
