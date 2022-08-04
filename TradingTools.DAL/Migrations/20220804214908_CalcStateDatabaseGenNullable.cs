491e54981a1e6e7a451ab4114cce480c62ad33fe
cswirl <jay.angeles@protonmail.com>
2022-08-04 17:57:03 -0400

    Add-Migration CalcStateDatabaseGenNullable. Update-Database

---
+++ TradingTools.DAL/Migrations/20220804214908_CalcStateDatabaseGenNullable.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TradingTools.DAL.Migrations
+{
+    public partial class CalcStateDatabaseGenNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Side",
+                table: "CalculatorState",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "OpeningTradingFee",
+                table: "CalculatorState",
+                type: "decimal(18,6)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "OpeningTradingCost",
+                table: "CalculatorState",
+                type: "decimal(18,6)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "InterestCost",
+                table: "CalculatorState",
+                type: "decimal(18,6)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BorrowAmount",
+                table: "CalculatorState",
+                type: "decimal(18,6)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Side",
+                table: "CalculatorState",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "OpeningTradingFee",
+                table: "CalculatorState",
+                type: "decimal(18,6)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "OpeningTradingCost",
+                table: "CalculatorState",
+                type: "decimal(18,6)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "InterestCost",
+                table: "CalculatorState",
+                type: "decimal(18,6)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BorrowAmount",
+                table: "CalculatorState",
+                type: "decimal(18,6)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldNullable: true);
+        }
+    }
+}
