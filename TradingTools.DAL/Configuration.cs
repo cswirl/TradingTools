0ffc133c42b12060b98d9934334b3bfbce6ee503
cswirl <jay.angeles@protonmail.com>
2022-08-09 19:20:53 -0400

    SQLite lubrication
    - Add SQLite database provider

--- TradingTools.DAL/Configuration.cs
+++ TradingTools.DAL/Configuration.cs
@@ -11,8 +11,8 @@ namespace TradingTools.DAL
     {
         private const string connectionStringName = "TradingToolsConnectionString";
 
-        private const string sqlServerConnectionString = @"Server=localhost\SQLEXPRESS;Database=TradingToolsDB;Trusted_Connection=True;";
-        //private const string sqlServerConnectionString = @"Server=localhost\SQLEXPRESS;Database=TradingToolsProd;Trusted_Connection=True;";
+        //private const string sqlServerConnectionString = @"Server=localhost\SQLEXPRESS;Database=TradingToolsDB;Trusted_Connection=True;";
+        private const string sqlServerConnectionString = @"Server=localhost\SQLEXPRESS;Database=TimeHorizonTradingToolsProd;Trusted_Connection=True;";
         private const string sqliteConnectionString = @"Data source=TradingTools.db";
 
         public static void SetConnectionString(string connectionString)
