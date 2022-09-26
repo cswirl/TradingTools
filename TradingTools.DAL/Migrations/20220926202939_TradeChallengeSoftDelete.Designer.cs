6ca317bf274cb6d520152c5eb18a1a5a57fec243
cswirl <jay.angeles@protonmail.com>
2022-09-26 16:42:29 -0400

    Add-Migration TradeChallengeSoftDelete

--- TradingTools.DAL/Migrations/20220926202939_TradeChallengeSoftDelete.Designer.cs
+++ TradingTools.DAL/Migrations/20220926202939_TradeChallengeSoftDelete.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TradingTools.DAL;
 
 namespace TradingTools.DAL.Migrations
 {
     [DbContext(typeof(TradingToolsDbContext))]
-    partial class TradingToolsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220926202939_TradeChallengeSoftDelete")]
+    partial class TradeChallengeSoftDelete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
