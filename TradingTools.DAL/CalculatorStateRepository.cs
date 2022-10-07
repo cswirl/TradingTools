1d70213b34faa76bf469af1c5935726743bf44d8
cswirl <jay.angeles@protonmail.com>
2022-10-07 02:24:49 -0400

    Add Repository classes and interfaces

---
+++ TradingTools.DAL/CalculatorStateRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradingTools.Trunk.Contracts;
+using TradingTools.Trunk.Entity;
+
+namespace TradingTools.Repository
+{
+    public class CalculatorStateRepository : RepositoryBase<CalculatorState>, ICalculatorStateRepository
+    {
+        public CalculatorStateRepository(TradingToolsDbContext repositoryContext) : base(repositoryContext)
+        {
+        }
+    }
+}
---
+++ TradingTools.DAL/RepositoryManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradingTools.Trunk.Contracts;
+
+namespace TradingTools.Repository
+{
+    public sealed class RepositoryManager : IRepositoryManager
+    {
+		private readonly TradingToolsDbContext _repositoryContext;
+		private readonly Lazy<ITradeRepository> _tradeRepository;
+		private readonly Lazy<ICalculatorStateRepository> _calculatorStateRepository;
+
+		public RepositoryManager(TradingToolsDbContext repositoryContext)
+		{
+			_repositoryContext = repositoryContext;
+			_tradeRepository = new(() => new TradeRepository(repositoryContext));
+			_calculatorStateRepository = new(() => new CalculatorStateRepository(repositoryContext));
+		}
+
+		public ITradeRepository Trade => _tradeRepository.Value;
+		public ICalculatorStateRepository CalculatorState => _calculatorStateRepository.Value;
+
+		public void Save() => _repositoryContext.SaveChanges();
+	}
+}
